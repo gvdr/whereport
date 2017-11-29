6bdda283366ec7876359ec9a8bc90342057b9a1f
gvdr <giulio.riva@stat.ubc.ca>
2017-11-29 13:43:39 -0800

    better docs for dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Geographical location and Iata, ICAO codes for 8965 international airports.
+#'
+#' @format A data frame with 8965 rows and 6 variables:
+#' \describe{
+#'   \item{IATA}{IATA code of the airport}
+#'   \item{ICAO}{ICAO code of the airport}
+#'   \item{Airport_name}{Common name of the airport}
+#'   \item{Location_served}{Geographical location for the airport}
+#'   \item{Time}{Time zone of the location served}
+#'   \item{DST}{Months in which the Daylight Saving Time is observed}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code/}
+"Data_IATA"
